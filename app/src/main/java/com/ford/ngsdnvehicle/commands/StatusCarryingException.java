b10bc8a365ab26a077fc833ff65db90e5d546cd5
DustinThostenson <dustin@delta3consulting.com>
2021-03-01 13:07:11 -0600

    implementing exception and base exception

--- app/src/main/java/com/ford/ngsdnvehicle/commands/StatusCarryingException.java
+++ app/src/main/java/com/ford/ngsdnvehicle/commands/StatusCarryingException.java
@@ -1,7 +1,19 @@
 package com.ford.ngsdnvehicle.commands;
 
 public class StatusCarryingException extends Throwable {
+    private  Throwable theCause;
+    private int statusCode;
+
+    public StatusCarryingException(String valueOf) {
+        statusCode = Integer.parseInt(valueOf);
+    }
+
+    public StatusCarryingException(String valueOf, Throwable cause) {
+        statusCode = Integer.parseInt(valueOf);
+        theCause = cause;
+    }
+
     public int getStatusCode() {
-        return 0;
+        return statusCode;
     }
 }
